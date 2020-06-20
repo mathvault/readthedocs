87fa1590a65a9279a5e791201e8c693bf1b15824
Math Vault <info@mathvault.ca>
2020-06-19 23:52:30 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -3,11 +3,15 @@
 # These folders are copied to the documentation's HTML output
 html_static_path = ['_static']
 
+# Add custom css overrides
+def setup(app):
+  app.add_stylesheet( "custom.css" )
+
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
-html_css_files = [
-    'css/custom.css',
-]
+# html_css_files = [
+#    'css/custom.css',
+# ]
 
 html_js_files = [
     'js/custom.js',
